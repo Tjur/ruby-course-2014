47e65a8305d649a2416726e25135be750591769f
Tjur <tommy.jurkiewicz@gmail.com>
2014-10-20 12:07:16 +0200

    [#4] initial commit

---
+++ article.rb
@@ -0,0 +1,50 @@
+class Article
+	def initialize(title, content, author = "")
+	  @title = title
+	  @content = content
+	  @author = author
+	  @likes = @dislikes = 0
+	  @created_at = Time.now	
+	end
+
+	def like!
+	  @like += 1
+	end
+
+	def dislike!
+	  @dislike += 1
+	end
+
+	def positive_votes
+	  like! - dislike!
+	end
+
+	def votes
+	  @likes + @dislikes
+	end
+
+	def shortened_to(limit)
+	  return @content if limit > @content.size
+	  @content[0...limit-3] << "..."
+	end
+
+	def include?(pattern)
+	  @content.include?(pattern)
+	end
+
+	def words
+	  @content.split(/\W+/)
+	end
+
+	def distinct_words
+		words.uniq!
+	end
+
+	def title
+	  @title
+	end
+
+	def author
+	  @author
+	end
+end
