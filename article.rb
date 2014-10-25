be2d125959f0b124899b6e3a678104eeb778f732
Tjur <tommy.jurkiewicz@gmail.com>
2014-10-25 12:25:36 +0200

    Indentations converted to spaces

--- article.rb
+++ article.rb
@@ -1,42 +1,42 @@
 class Article
-	attr_reader :title, :author
+  attr_reader :title, :author
 
-	def initialize(title, content, author = "")
-	  @title, @content, @author = title, content, author
-	  @likes = @dislikes = 0
-	  @created_at = Time.now
-	end
+  def initialize(title, content, author = "")
+    @title, @content, @author = title, content, author
+    @likes = @dislikes = 0
+    @created_at = Time.now
+  end
 
-	def like!
-	  @likes += 1
-	end
+  def like!
+    @likes += 1
+  end
 
-	def dislike!
-	  @dislikes += 1
-	end
+  def dislike!
+    @dislikes += 1
+  end
 
-	def positive_votes
-	  @likes - @dislikes
-	end
+  def positive_votes
+    @likes - @dislikes
+  end
 
-	def votes
-	  @likes + @dislikes
-	end
+  def votes
+    @likes + @dislikes
+  end
 
-	def shortened_to(limit)
-	  return @content if limit > @content.size
-	  @content[0...limit-3] << "..."
-	end
+  def shortened_to(limit)
+    return @content if limit > @content.size
+    @content[0...limit-3] << "..."
+  end
 
-	def include?(pattern)
-	  @content.include?(pattern)
-	end
+  def include?(pattern)
+    @content.include?(pattern)
+  end
 
-	def words
-	  @content.split(/\W+/)
-	end
+  def words
+    @content.split(/\W+/)
+  end
 
-	def distinct_words
-		words.uniq!
-	end
+  def distinct_words
+    words.uniq!
+  end
 end
