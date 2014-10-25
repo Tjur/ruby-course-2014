d45e7b0b14f5f564c540fbc87b844186aa2cd5f1
Tjur <tommy.jurkiewicz@gmail.com>
2014-10-25 12:26:35 +0200

    [#5] Indentations converted to spaces

--- article_manager.rb
+++ article_manager.rb
@@ -1,47 +1,47 @@
 require './article.rb'
 
 class ArticleManager
-	def initialize(*articles)
-	  @articles = articles
-	end
+  def initialize(*articles)
+    @articles = articles
+  end
 
-	def worst_articles
-	  @articles.sort{ |a| a.positive_votes }
-	end
+  def worst_articles
+    @articles.sort{ |a| a.positive_votes }
+  end
 
-	def best_articles
-	  worst_articles.reverse
-	end
+  def best_articles
+    worst_articles.reverse
+  end
 
-	def worst_article
-	  worst_articles.first
-	end
+  def worst_article
+    worst_articles.first
+  end
 
-	def best_article
-	  best_articles.first
-	end
+  def best_article
+    best_articles.first
+  end
 
-	def most_popular_article
-	  @articles.sort{ |a| a.votes }.last
-	end
+  def most_popular_article
+    @articles.sort{ |a| a.votes }.last
+  end
 
-	def include?(pattern)
-	  @articles.keep_if{ |a| a.include?(pattern) }
-	end
+  def include?(pattern)
+    @articles.keep_if{ |a| a.include?(pattern) }
+  end
 
-	def authors
-	  @articles.collect{ |a| a.author }.uniq!
-	end
+  def authors
+    @articles.collect{ |a| a.author }.uniq!
+  end
 
-	def number_of_authors
-	  authors.size
-	end
+  def number_of_authors
+    authors.size
+  end
 
-	def votes
-	  @articles.collect{ |a| a.votes }.reduce(:+)
-	end
+  def votes
+    @articles.collect{ |a| a.votes }.reduce(:+)
+  end
 
-	def to_s
-	  @articles.each{ |a| puts "Title: \"#{a.title}\"" }
-	end
+  def to_s
+    @articles.each{ |a| puts "Title: \"#{a.title}\"" }
+  end
 end
