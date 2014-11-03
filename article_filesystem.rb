6ec4fc0696b7c87c02559c094edf64c2730d4f18
Tjur <tommy.jurkiewicz@gmail.com>
2014-11-03 12:23:14 +0100

    [#12],[#13] Article Filesystem

---
+++ article_filesystem.rb
@@ -0,0 +1,16 @@
+class ArticleFilesystem
+  ARTICLES_FOLDER = "./articles"
+
+  def self.create_file(art)
+    file = File.new(ARTICLES_FOLDER + "/" + art.title.gsub(/\W+/, "_"), "w")
+    file.write("#{art.author}|#{art.title}|#{art.content}|#{art.likes}|#{art.dislikes}")
+    file.close
+  end
+
+  def self.get_article(path)
+    tab = File.open(path, &:readline).split(/\|/)
+    art = Article.new(tab[1], tab[2], tab[0])
+    tab[3].times { art.like! }
+    tab[4].times { art.dislike! }
+    art
+end
